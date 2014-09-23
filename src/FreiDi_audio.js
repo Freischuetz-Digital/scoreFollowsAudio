b63427eb5faad2983467d938c1d5fb74f2db38a0
Benjamin W. Bohl <bohl@edirom.de>
2014-09-23 16:05:48 +0200

    added JS for retrieving audio & measures

---
+++ src/FreiDi_audio.js
@@ -0,0 +1,46 @@
+/*
+ * not used
+ */
+function appendAudio() {
+  var audio = new Audio();
+  audio.id = 'track_js'
+  audio.src = 'http://localhost:8000/Weber_Freischuetz-06_FreiDi.mp3';
+  audio.controls = true;
+  document.getElementById("audioDiv").appendChild(audio);
+  console.log('audio appended');
+};
+/*
+ * used
+ */
+
+function getMeasure(time){
+  var bestDiff = 0;
+  var minDiff;
+  var bestIndex;
+  var i;
+  var cur;
+  for (i=0; i < json.measures.length; i++){
+    cur = json.measures[i];
+    diff = cur.timestamp - time;
+    console.log(cur.timestamp);
+    console.log(diff);
+    if(diff < bestDiff){
+      minDiff = diff;
+      bestIndex = i +1; //+1 um die tatsächliche Taktzahl zu errechnen
+    }
+  }
+  $("#measureCount").text(bestIndex);
+  console.log("bestIndex: ");
+  console.log(bestIndex);
+  console.log("minDiff: ");
+  console.log(minDiff);
+};
+
+/*
+ * research
+ */
+/*var mediaElement = document.getElementById('track');
+mediaElement.seekable.start();  // Returns the starting time (in seconds)
+mediaElement.seekable.end();    // Returns the ending time (in seconds)
+mediaElement.currentTime = 122; // Seek to 122 seconds
+mediaElement.played.end();      // Returns the number of seconds the browser has played*/
